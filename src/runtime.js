2c7732ab2eb7a4eee0cc19675ea886cc000096a4
Jacob Stern <jacob.stern@outlook.com>
2019-04-08 21:54:47 -0700

    Hard fork runtime.js

--- src/runtime.js
+++ src/runtime.js
@@ -1,9 +1,11 @@
 import * as IncrementalDOM from 'incremental-dom';
-import HandlebarsIDOM from '../lib/runtime';
+import HandlebarsIDOM from '../lib/index.runtime';
 
 if (window.IncrementalDOM == null) {
-  // TODO: Allow user-defined IncrementalDOM global as well To what extent do we
-  // expect this library to rely on a specific version of incremental-dom?
+  // TODO: Allow user-defined IncrementalDOM global as well.
+
+  // To what extent do we expect this library to rely on a specific version of
+  // incremental-dom?
   window.IncrementalDOM = IncrementalDOM;
 }
 
