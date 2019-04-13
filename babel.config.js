fcf92c802287706fc4ebea039fe723e871809110
Jacob Stern <jacob.stern@outlook.com>
2019-04-13 15:00:02 -0700

    Use babel-plugin-add-module-exports

--- babel.config.js
+++ babel.config.js
@@ -14,5 +14,13 @@ module.exports = api => {
       ['@babel/preset-env', presetEnvOptions],
       '@babel/preset-typescript',
     ],
+    plugins: [
+      [
+        'add-module-exports',
+        {
+          addDefaultProperty: true,
+        },
+      ],
+    ],
   };
 };
